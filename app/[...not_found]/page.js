e7a9f2f0530bc9dc8e96462a38f9113d7384ac94
Mahesh Kotha <Mahesh.Kotha@dazn.com>
2024-09-16 16:41:57 +0530

    page not found page created

---
+++ app/[...not_found]/page.js
@@ -0,0 +1,14 @@
+
+export default function Error() {
+  return (
+    <div style={{height: "50vh", 
+      display: "flex", 
+      alignItems: "center", 
+      justifyContent:"center",
+      flexDirection: "column"
+      }}>
+      <h2>OOOPs Page not found !...</h2>
+      <p>try after some time</p>
+    </div>
+  )
+}
